24c62d4ab5a30f6a5e2e072b44fcc507a9305320
Lucky Otono <otonolucky4@gmail.com>
2023-07-12 13:01:42 +0100

    Appointment-Sheduler commit

---
+++ AppointmentScheduling/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using AppointmentScheduling.Models;
+using AppointmentScheduling.Services;
+using AppointmentScheduling.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace AppointmentScheduling.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IAppointmentService _appointmentService;
+
+        public HomeController(ILogger<HomeController> logger, IAppointmentService appointmentService)
+        {
+            _logger = logger;
+            _appointmentService = appointmentService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Duration = Helper.GetTimeDropDown();
+            ViewBag.DoctorList = _appointmentService.GetDoctorList();
+            ViewBag.PatientList = _appointmentService.GetPatientList();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AppointmentScheduling/Utility/EmailSender.cs
@@ -0,0 +1,35 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            MailjetClient client = new MailjetClient("8667564b1fc4009476683f371403d0e4", "4ff81c9a7b4c4342ad8e386fb7ef96a5")
+            {
+
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+           .Property(Send.FromEmail, "onuhchukwuma21@gmail.com")
+           .Property(Send.FromName, "Appointment Scheduler")
+           .Property(Send.Subject, subject)
+           .Property(Send.HtmlPart, htmlMessage)
+           .Property(Send.Recipients, new JArray {
+                new JObject {
+                 {"Email", email}
+                 }
+               });
+            MailjetResponse response = await client.PostAsync(request);
+
+
+        }
+    }
+}
